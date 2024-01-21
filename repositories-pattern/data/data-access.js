000116dd89046e1830c28a74d258634e610f0171
NDK-ITer <116534879+NDK-ITer@users.noreply.github.com>
2024-01-21 12:59:38 +0700

    re-inital

--- repositories-pattern/data/data-access.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/initialProject-nodejs', { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
-});
-
-module.exports = mongoose;
